0b50ac466d46a3485d77c3dd0f6e429a7b217cba
Tovgin <tovgin2004@gmail.com>
2022-12-01 20:11:41 +0200

    Додано абстрактний клас

---
+++ src/AbstractCalc.java
@@ -0,0 +1,6 @@
+public abstract class AbstractCalc {
+    protected abstract String Add() ;
+    protected abstract String Sub();
+    protected abstract String Mul();
+    protected abstract String Div();
+}
